c12878426267bc2b7dba092e9b58f53b93f41f8e
T. <105440207+School-Chromebook663@users.noreply.github.com>
2025-01-23 12:12:47 -0500

    othertwekwoenowenf

--- legacy/rv1-alpha/assets/js/error.js
+++ legacy/rv1-alpha/assets/js/error.js
@@ -1,9 +1,9 @@
 var count = (function () {
 	var counter = 0;
 	return function () {
-        return counter +=1;
-    }
+		return counter +=1;
+	}
 })();
-function displaycount(){
+function displaycount() {
 	document.getElementById("carrier").innerHTML = count();
 }
