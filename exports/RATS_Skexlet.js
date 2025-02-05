981c30ec68309cfd02263d88749c72ad7f2dca7b
Scadaddle <105403291+Skadinkle@users.noreply.github.com>
2025-02-05 03:29:54 +0000

    Stop tracking exports/ folder

--- exports/RATS_Skexlet.js
+++
@@ -1,9 +0,0 @@
-javascript:(function() {
-	if (!document.getElementById("skexlet")) {
-		const skexlet = document.createElement("iframe");
-		skexlet.id = "skexlet";
-		skexlet.src = "https://skadinkle.github.io/rats";
-		skexlet.setAttribute("style", "position:fixed; top:0; left:0; width:100%; height:100%; border:none; z-index:9999999;");
-		document.body.appendChild(skexlet);
-	}
-})();
