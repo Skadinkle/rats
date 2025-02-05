a305fc5b43f818978860a718ed9bfd752bf54939
Scadaddle <105403291+Skadinkle@users.noreply.github.com>
2025-02-05 03:30:52 +0000

    bybby

---
+++ exports/RATS_Skexlet.js
@@ -0,0 +1,9 @@
+javascript:(function() {
+	if (!document.getElementById("skexlet")) {
+		const skexlet = document.createElement("iframe");
+		skexlet.id = "skexlet";
+		skexlet.src = "https://skadinkle.github.io/rats";
+		skexlet.setAttribute("style", "position:fixed; top:0; left:0; width:100%; height:100%; border:none; z-index:9999999;");
+		document.body.appendChild(skexlet);
+	}
+})();
